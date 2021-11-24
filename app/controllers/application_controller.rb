71316baadde78b0e07e1421cb8bb201af3aef417
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-11-24 14:52:45 +0900

    set controller(index,new,create,strong_ method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  def index
+    @user = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @usar.save
+    redirect_to '/'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password_confirmation)
+  end
 end
+
+
