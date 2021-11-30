a4706a987a7882f42a9b47d94f13b8b4adf16b9b
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-11-30 10:58:10 +0900

    set controller for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,11 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    @user = User.new(user_params)
+    @user.save
+    log_in @user
+    redirect_to @user
+
 end
