41bc3f240709ced95e798bac704e0e3e20b54f8d
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-11-30 13:56:27 +0900

    set tweet controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+  get 'tweets/new'
+  get 'tweets/create'
+  get 'tweets/show'
+  get 'tweets/edit'
+  get 'tweets/destroy'
   get 'sessions/new'
   get 'users/index'
   get 'users/new'
