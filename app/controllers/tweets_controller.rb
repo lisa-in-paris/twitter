d04bb6f8e655c95e4caa599de7ecbbb0e51d9cde
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-11-30 17:40:18 +0900

    set controller for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,19 +1,26 @@
 class TweetsController < ApplicationController
   def index
+    @tweet = Tweet.all
   end
 
   def new
+    @user = User.find(params[:user_id])
   end
 
   def create
-  end
-
-  def show
-  end
-
-  def edit
+    @user = User.find(params[:user_id])
+    @user.tweets.create(tweet_params)
+    redirect_to tweets_index_path
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    if @tweet.destroy
+    redirect_to "/",flash: {danger: "投稿を削除しました"}
   end
-end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
